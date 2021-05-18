6c14525f965590470feab9d6416ae506f8655caf
MR7_GH <m.gh138080@gmail.com>
2021-05-18 18:30:26 +0430

    getting card from host - Be carefull if you want to add new cards you have to change the resource in the host and make some changes in code!

--- code/src/Controller.java
+++ code/src/Controller.java
@@ -1,14 +1,19 @@
-
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.io.File;
 import java.util.Scanner;
 
 public class Controller {
     public static int menuNumber = 1;
     public static Scanner scanner = new Scanner(System.in);
+
     static {
         System.out.println("Login and SignUp Menu!");
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
         checkFolders();
         while (true) {
             String input = scanner.nextLine();
@@ -20,23 +25,44 @@ public class Controller {
                     MainMenu.mainMenuController(input);
                     break;
                 case 6:
-                    Profile.profileControl(MainMenu.player,input);
+                    Profile.profileControl(MainMenu.player, input);
                     break;
                 case 5:
                     Scoreboard.scoreboardControl(input);
                     break;
                 case 4:
-                    Deck.deckControl(input,MainMenu.player);
+                    Deck.deckControl(input, MainMenu.player);
             }
         }
     }
 
-    private static void checkFolders() {
+    private static void checkFolders() throws Exception {
         File file = new File(System.getProperty("user.dir") + "\\src\\users");
         file.mkdir();
         file = new File(System.getProperty("user.dir") + "\\src\\decks");
         file.mkdir();
         file = new File(System.getProperty("user.dir") + "\\src\\cards");
         file.mkdir();
+        //I put 68 here because we have 68 cards
+        //change this num if you want to add some new cards or remove them
+        if(file.list().length != 68){
+            String[] filesNames = file.list();
+            for(String s : filesNames){
+                File currentFile = new File(file.getPath(),s);
+                currentFile.delete();
+            }
+            URL url = new URL("http://mobinghorbani.ir/Yu-Gi-Oh/cards.zip");
+            downloadFile(url, "src\\cards\\cards.zip");
+            UnzipClass.unzip("src\\cards\\cards.zip","src\\cards");
+            file = new File("src\\cards\\cards.zip");
+            file.delete();
+        }
     }
+
+    public static void downloadFile(URL url, String fileName) throws Exception {
+        try (InputStream in = url.openStream()) {
+            Files.copy(in, Paths.get(fileName));
+        }
+    }
+
 }
